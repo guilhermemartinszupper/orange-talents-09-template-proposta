07529badb6807d8b2aa48278b1f5997c4c554058
Guilherme Martins <guilherme.martins@zup.com.br>
2021-10-27 10:33:39 -0300

    4.11.7.1 - Melhorando a visibilidade da nossa aplicação para equipe de operação (Health Check)

--- proposta/src/main/java/br/com/zupedu/gui/desafioproposta/proposta/analise/SolicitacaoAnaliseClient.java
+++ proposta/src/main/java/br/com/zupedu/gui/desafioproposta/proposta/analise/SolicitacaoAnaliseClient.java
@@ -3,7 +3,7 @@ package br.com.zupedu.gui.desafioproposta.proposta.analise;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(value = "analise-api",url = "http://localhost:9999")
+@FeignClient(value = "analise-api",url = "${analise.host}")
 public interface SolicitacaoAnaliseClient {
     @PostMapping("/api/solicitacao")
     public SolicitacaoAnaliseResponse solicitacao(SolicitacaoAnaliseRequest request);
