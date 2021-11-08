7f2a53efd8eb4e07aa9058d9524b8fc9858636d7
Guilherme Martins <guilherme.martins@zup.com.br>
2021-11-08 09:57:16 -0300

    4.11.21.1 - Rodando o Prometheus

---
+++ proposta/src/main/java/br/com/zupedu/gui/desafioproposta/security/ResourceServerConfig.java
@@ -0,0 +1,27 @@
+package br.com.zupedu.gui.desafioproposta.security;
+
+import org.springframework.context.annotation.*;
+import org.springframework.http.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.*;
+
+@Configuration
+@EnableWebSecurity
+@Profile(value = {"dev","prod"})
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.GET, "/actuator/prometheus").permitAll()
+                        .antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_proposta-scope")
+                        .antMatchers(HttpMethod.POST, "/propostas/**").hasAuthority("SCOPE_proposta-scope")
+                        .antMatchers(HttpMethod.POST, "/biometrias/**").hasAuthority("SCOPE_proposta-scope")
+                        .antMatchers(HttpMethod.POST, "/actuator/**").hasAuthority("SCOPE_proposta-scope")
+                        .anyRequest().authenticated()
+        ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+}
